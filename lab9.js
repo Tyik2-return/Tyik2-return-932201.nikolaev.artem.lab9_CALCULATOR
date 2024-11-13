f022d1d590fa13e855779d23eade2cb60e8a09b3
Артем <nickolawan2004@gmail.com>
2024-11-13 19:07:19 +0700

    Update lab9.js

--- lab9.js
+++ lab9.js
@@ -10,60 +10,86 @@ buttons.forEach((button) => {
         break;
       case "=":
         try {
+          if (/[+\-*/%]/.test(display.innerText.slice(-1))) {
+            display.innerText += display.innerText.slice(0, -1);
+          }
           display.innerText = eval(display.innerText);
         } catch (e) {
           display.innerText = "Error!";
         }
         break;
       case "+/-":
-        display.innerText*=-1;
+        display.innerText *= -1;
         break;
       case "%":
         let passedText = display.innerText + "/100";
         display.innerText = eval(passedText);
         break;
-        case "delite":
-            display.innerText = display.innerText.slice(0, -1); // Удаление последнего символа
-            if (display.innerText === "") {
-              display.innerText = "0"; // Если строка пуста, устанавливаем 0
-            }
-    break;
+      case "delite":
+        display.innerText = display.innerText.slice(0, -1);
+        if (display.innerText === "") {
+          display.innerText = "0";
+        }
+        break;
       default:
-        if (display.innerText === "0" && e.target.innerText !== ".") {
-          display.innerText = e.target.innerText;
-        } else {
-          display.innerText += e.target.innerText;
+        if (/[0-9.]/.test(e.target.innerText)) {
+          if (e.target.innerText === "." && display.innerText.includes(".")) {
+            return;
+          }
+          if (display.innerText === "0" && e.target.innerText !== ".") {
+            display.innerText = e.target.innerText;
+          } else {
+            display.innerText += e.target.innerText;
+          }
+        } else if (/[+\-*/%]/.test(e.target.innerText)) {
+          if (/[+\-*/%]/.test(display.innerText.slice(-1))) {
+            display.innerText = display.innerText.slice(0, -1) + e.target.innerText;
+          } else {
+            display.innerText += e.target.innerText;
+          }
         }
     }
   });
 });
 
-document.addEventListener('keydown', (event) => {
-    const key = event.key;
-  
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
 
-    if (/\d|\.|\+|\-|\*|\/|\%/.test(key)) {     // Проверка, является ли нажатая клавиша цифрой, оператором или точкой
+  if (/\d|\.|\+|\-|\*|\/|\%/.test(key)) {
+    if (key === ".") {
+      if (display.innerText.includes(".")) {
+        return;
+      }
+    }
 
-      if (display.innerText === "0" && key !== ".") {       // Если да, добавить символ в дисплей
+    if (/[+\-*/%]/.test(key)) {
+      if (/[+\-*/%]/.test(display.innerText.slice(-1))) {
+        display.innerText = display.innerText.slice(0, -1) + key;
+      } else {
+        display.innerText += key;
+      }
+    } else {
+      if (display.innerText === "0" && key !== ".") {
         display.innerText = key;
       } else {
         display.innerText += key;
       }
-    } else if (key === 'Enter') {
-
-      try {
-        display.innerText = eval(display.innerText);       // Если нажата клавиша Enter, выполнить вычисление
-      } catch (e) {
-        display.innerText = "Error!";
+    }
+  } else if (key === "Enter") {
+    try {
+      if (/[+\-*/%]/.test(display.innerText.slice(-1))) {
+        display.innerText += display.innerText.slice(0, -1);
       }
-    } else if (key === 'Backspace') {
-
-      display.innerText = display.innerText.slice(0, -1);       // Если нажата клавиша Backspace, удалить последний символ
-      if (display.innerText === "") {
-        display.innerText = "0";
-      }
-    } else if (key === 'c') {       // Если нажата клавиша 'c', очистить дисплей
-
+      display.innerText = eval(display.innerText);  
+    } catch (e) {
+      display.innerText = "Error!";
+    }
+  } else if (key === "Backspace") {
+    display.innerText = display.innerText.slice(0, -1);  
+    if (display.innerText === "") {
       display.innerText = "0";
-    } 
-  });
+    }
+  } else if (key === "c") {
+    display.innerText = "0";
+  }
+});
